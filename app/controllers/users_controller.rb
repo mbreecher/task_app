27f012ba10b43f1c564b1cc457f0415b21505709
Mike Breecher <michaelbreecher@gmail.com>
2013-11-03 21:48:46 -0700

    finished adding before filters to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: #[:edit, :update]
   
   def show
   	@user = User.find(params[:id])
@@ -6,6 +8,21 @@ class UsersController < ApplicationController
   
   def new
   	@user = User.new
+  end
+
+  def edit
+    #remove with addition of before_action filter @user = User.find(params[:id])
+  end
+
+  def update
+    # remove with addition of before_action filter @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #handle a successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -24,4 +41,22 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    #before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    #which is equivalent to:
+    #unless signed_in?
+    #  flash[:notice] = "Please sign in."
+    #  redirect_to signin_url
+    #end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
