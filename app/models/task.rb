ecdfe52d9dbdd0ff749991628729ba78868e13bd
Mike Breecher <michaelbreecher@gmail.com>
2013-12-17 14:06:33 -0700

    added ability to make customer-less task

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,12 +3,11 @@ class Task < ActiveRecord::Base
 	belongs_to :customer, class_name: Customer, foreign_key: :customer_id
 	belongs_to :user, class_name: User, foreign_key: :csm_id
 	validates :name, presence: true, length: {maximum: 50}
-	validates :customer_id, presence: true
 	validates :due_date, presence: true
 
 	def self.search(search)
 		if search
-		where 'name LIKE ?', "%#{search}%"
+			where 'name LIKE ?', "%#{search}%"
 		else
 			scoped
 		end
