26800dd37791ca5f469412ef9fb3b1903626cbd8
Mike Breecher <michaelbreecher@gmail.com>
2014-01-08 10:13:28 -0700

    removed task character limit

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
 	#belongs_to :user
 	belongs_to :customer, class_name: Customer, foreign_key: :customer_id
 	belongs_to :user, class_name: User, foreign_key: :csm_id
-	validates :name, presence: true, length: {maximum: 50}
+	validates :name, presence: true
 	validates :due_date, presence: true
 
 	def self.search(search)
